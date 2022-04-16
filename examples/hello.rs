dcc4486330a6710701aa8606b096fa7d75685422
Laz <lazh@fastmail.com>
2022-04-16 15:46:56 -0700

    first working version

---
+++ examples/hello.rs
@@ -0,0 +1,33 @@
+use crossterm::event::{KeyCode, KeyEvent};
+use rustea::{quit_cmd, App, Command, Message};
+
+struct Model {
+    last_key: Option<char>,
+}
+
+impl App for Model {
+    fn update(&mut self, msg: Message) -> Option<Command> {
+        if let Ok(key_event) = msg.downcast::<KeyEvent>() {
+            match key_event.code {
+                KeyCode::Char('q') => return Some(quit_cmd),
+                KeyCode::Char(c) => {
+                    self.last_key = Some(c);
+                    return None;
+                }
+                _ => return None,
+            }
+        };
+
+        None
+    }
+
+    fn view(&self) -> String {
+        format!("Hello! You pressed: {:?}", self.last_key)
+    }
+}
+
+fn main() {
+    let model = Model { last_key: None };
+
+    rustea::run(model).unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,109 @@
+use std::{
+    any::Any,
+    io::{stdout, Result, Stdout},
+    sync::mpsc::{self, Sender},
+    thread,
+};
+
+use crossterm::{
+    cursor,
+    event::{read, Event},
+    execute,
+    style::Print,
+    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
+    ExecutableCommand,
+};
+
+pub trait App {
+    fn init(&self) -> Option<Command> {
+        None
+    }
+
+    fn update(&mut self, msg: Message) -> Option<Command>;
+    fn view(&self) -> String;
+}
+
+pub type Message = Box<dyn Any + Send>;
+
+pub type Command = fn() -> Option<Message>;
+
+struct Quit;
+pub fn quit_cmd() -> Option<Message> {
+    Some(Box::new(Quit))
+}
+
+pub fn run(app: impl App) -> Result<()> {
+    let mut app = app;
+    let mut stdout = stdout();
+
+    let (msg_tx, msg_rx) = mpsc::channel::<Message>();
+    let msg_tx2 = msg_tx.clone();
+
+    let (cmd_tx, cmd_rx) = mpsc::channel::<Command>();
+    let cmd_tx2 = cmd_tx.clone();
+
+    thread::spawn(move || {
+        loop {
+            match read().unwrap() {
+                Event::Key(event) => msg_tx.send(Box::new(event)).unwrap(),
+                // TODO: handle these events
+                _ => panic!("unexpected event"),
+            }
+        }
+    });
+
+    thread::spawn(move || {
+        let cmd = cmd_rx.recv().unwrap();
+        thread::spawn(move || {
+            if let Some(msg) = cmd() {
+                msg_tx2.send(msg).unwrap();
+            }
+        })
+    });
+
+    initialize(&mut stdout, &app, cmd_tx2)?;
+
+    loop {
+        let msg = msg_rx.recv().unwrap();
+        if msg.is::<Quit>() {
+            break;
+        }
+
+        if let Some(cmd) = app.update(msg) {
+            cmd_tx.send(cmd).unwrap();
+        }
+
+        paint_screen(&mut stdout, &app.view())?;
+    }
+
+    deinitialize(&mut stdout)
+}
+
+fn initialize(stdout: &mut Stdout, app: &impl App, cmd_tx: Sender<Command>) -> Result<()> {
+    if let Some(cmd) = app.init() {
+        cmd_tx.send(cmd).unwrap();
+    }
+
+    enable_raw_mode()?;
+    stdout.execute(cursor::Hide)?;
+    paint_screen(stdout, &app.view())
+}
+
+fn paint_screen(stdout: &mut Stdout, s: &str) -> Result<()> {
+    for line in s.lines() {
+        execute!(
+            stdout,
+            cursor::MoveToColumn(1),
+            cursor::MoveUp(1),
+            Clear(ClearType::CurrentLine),
+            Print(line.to_owned() + "\r\n"),
+        )?;
+    }
+
+    Ok(())
+}
+
+fn deinitialize(stdout: &mut Stdout) -> Result<()> {
+    stdout.execute(cursor::Show)?;
+    disable_raw_mode()
+}
